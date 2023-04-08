464e1cf5f1508dfcd3e8581aa2e1d79ab0e6f2d2
Anna Li <Kristusha3@yandex.ru>
2023-04-08 23:12:00 +0300

    initial commit2

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,2 +1,7 @@
 public class CreditPaymentService {
+    public int calculate(int amount, double percentBid, int period) {
+        double monthBid = (percentBid / 100 ) / 12;
+        double payment = amount * (monthBid * Math.pow ((monthBid + 1), period) / (Math.pow ((monthBid + 1), period) - 1));
+        return (int) payment;
+    }
 }
